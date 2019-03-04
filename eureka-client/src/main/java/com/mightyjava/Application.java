4c94342c7513705ec6fdd5bb4bc965f0d08d168b
mightyjava <jeetparmar949@gmail.com>
2019-03-04 21:22:59 +0530

    initial import

---
+++ eureka-client/src/main/java/com/mightyjava/Application.java
@@ -0,0 +1,23 @@
+package com.mightyjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class Application {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
